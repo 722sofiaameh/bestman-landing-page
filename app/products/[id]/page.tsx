e33638b28601d5cb4050fba178479a703067ab0c
Sophia <sofiagabriel722@gmail.com>
2026-02-23 11:13:09 +0100

    fix: navabr fix

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -102,9 +102,7 @@ export default function ProductDetailsPage({ params }: ProductDetailsPageProps) 
 
   return (
     <div className="flex flex-col w-full items-start relative bg-white overflow-hidden">
-      <div className="w-full -translate-y-4 animate-fade-in opacity-0">
-        <Navbar />
-      </div>
+      <Navbar backgroundColor="bg-[#1180C5]" />
 
       <div className="w-full mt-16 sm:mt-20 md:mt-24 -translate-y-4 animate-fade-in opacity-0 [--animation-delay:200ms]">
 
