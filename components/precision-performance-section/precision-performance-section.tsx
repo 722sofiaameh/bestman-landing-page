b10165d024207f914341943a4900485f917bed5e
Sophia <sofiagabriel722@gmail.com>
2026-03-24 10:27:10 +0100

    feat: update scaling issue

--- components/precision-performance-section/precision-performance-section.tsx
+++ components/precision-performance-section/precision-performance-section.tsx
@@ -12,7 +12,7 @@ const coreValues = [
 
 export const PrecisionPerformanceSection = (): JSX.Element => {
   return (
-    <section className="bg-[#1180c5] pt-0 pb-8 md:pb-[50px] px-4 md:px-8 lg:px-[120px]">
+    <section className="bg-[#1180c5] pt-0 pb-8 mt-[-50px] md:mt-[-100px] lg:mt-0 md:pb-[50px] px-4 md:px-8 lg:px-[120px]">
       <div className="max-w-[1200px] mx-auto relative">
         <div className="grid grid-cols-1 lg:grid-cols-[400px_1fr] gap-6 md:gap-8 lg:gap-[94.7px] items-start">
 
